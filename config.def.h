ddc8944567b39ea4aba5693bc7da93452013c84a
Ghoyome <Ghoyome@protonmail.com>
2020-07-03 20:01:06 -0400

    patch: movestack

--- config.def.h
+++ config.def.h
@@ -81,12 +81,13 @@ static Key keys[] = {
 
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.0125} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.0125} },
+  { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+  { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.0125} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.0125} },
 
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
--- dwm.c
+++ dwm.c
@@ -152,6 +152,7 @@ static int applysizehints(Client *c, int *x, int *y, int *w, int *h, int interac
 static void arrange(Monitor *m);
 static void arrangemon(Monitor *m);
 static void attach(Client *c);
+static void movestack(const Arg *arg);
 static void attachbottom(Client *c);
 static void fullscreen(const Arg *arg);
 static void attachstack(Client *c);
@@ -509,6 +510,54 @@ attach(Client *c)
 	c->mon->clients = c;
 }
 
+void
+movestack(const Arg *arg) {
+  Client *c = NULL, *p = NULL, *pc = NULL, *i;
+
+  if(arg->i > 0) {
+    /* find the client after selmon->sel */
+    for(c = selmon->sel->next; c && (!ISVISIBLE(c) || c->isfloating); c = c->next);
+    if(!c)
+      for(c = selmon->clients; c && (!ISVISIBLE(c) || c->isfloating); c = c->next);
+
+  }
+  else {
+    /* find the client before selmon->sel */
+    for(i = selmon->clients; i != selmon->sel; i = i->next)
+      if(ISVISIBLE(i) && !i->isfloating)
+        c = i;
+    if(!c)
+      for(; i; i = i->next)
+        if(ISVISIBLE(i) && !i->isfloating)
+          c = i;
+  }
+  /* find the client before selmon->sel and c */
+  for(i = selmon->clients; i && (!p || !pc); i = i->next) {
+    if(i->next == selmon->sel)
+      p = i;
+    if(i->next == c)
+      pc = i;
+  }
+
+  /* swap c and selmon->sel selmon->clients in the selmon->clients list */
+  if(c && c != selmon->sel) {
+    Client *temp = selmon->sel->next==c?selmon->sel:selmon->sel->next;
+    selmon->sel->next = c->next==selmon->sel?c:c->next;
+    c->next = temp;
+
+    if(p && p != c)
+      p->next = c;
+    if(pc && pc != selmon->sel)
+      pc->next = selmon->sel;
+
+    if(selmon->sel == selmon->clients)
+      selmon->clients = c;
+    else if(c == selmon->clients)
+      selmon->clients = selmon->sel;
+
+    arrange(selmon);
+  }
+}
 void
 attachbottom(Client *c)
 {
