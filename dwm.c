d0e49e07a5344c909e153a38215c346e42e840e4
Ghoyome <Ghoyome@protonmail.com>
2020-07-05 19:41:35 -0400

    .

--- dwm.c
+++ dwm.c
@@ -219,7 +219,6 @@ static void show(Client *c);
 static void showhide(Client *c);
 static void sigchld(int unused);
 static void spawn(const Arg *arg);
-static void switchcol(const Arg *arg);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void tile(Monitor *);
@@ -1899,35 +1898,6 @@ spawn(const Arg *arg)
 		fprintf(stderr, "dwm: execvp %s", ((char **)arg->v)[0]);
 		perror(" failed");
 		exit(EXIT_SUCCESS);
-	}
-}
-
-void
-switchcol(const Arg *arg)
-{
-	Client *c, *t;
-	int col = 0;
-	int i;
-
-	if (!selmon->sel)
-		return;
-	for (i = 0, c = nexttiled(selmon->clients); c ;
-	     c = nexttiled(c->next), i++) {
-		if (c == selmon->sel)
-			col = (i + 1) > selmon->nmaster;
-	}
-	if (i <= selmon->nmaster)
-		return;
-	for (c = selmon->stack; c; c = c->snext) {
-		if (!ISVISIBLE(c))
-			continue;
-		for (i = 0, t = nexttiled(selmon->clients); t && t != c;
-		     t = nexttiled(t->next), i++);
-		if (t && (i + 1 > selmon->nmaster) != col) {
-			focus(c);
-			restack(selmon);
-			break;
-		}
 	}
 }
 
