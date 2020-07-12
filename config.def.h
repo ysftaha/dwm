1cb1ed66f1861db5e0c4eee0a7bf7d791d562178
Ghoyome <Ghoyome@protonmail.com>
2020-07-12 18:10:24 -0400

    .

--- config.def.h
+++ config.def.h
@@ -109,6 +109,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 
+  { MODKEY,                       XK_Print,	  spawn,		SHCMD("maim /home/base/sc-shot.png") },
+
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
