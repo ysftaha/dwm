6393a8d303148096996d53dfee0c618c02a43db4
Ghoyome <Ghoyome@protonmail.com>
2020-07-15 19:36:05 +0000

    .

--- config.def.h
+++ config.def.h
@@ -125,9 +125,10 @@ static Key keys[] = {
 
   { 0, XF86XK_MonBrightnessUp,	  spawn,		SHCMD("xbacklight -inc 1") },
 	{ 0, XF86XK_MonBrightnessDown,	spawn,		SHCMD("xbacklight -dec 1") },
-  { 0, XF86XK_AudioMute,       	  spawn,		SHCMD("pulsemixer --toggle-mute") },
-  { 0, XF86XK_AudioRaiseVolume, 	spawn,		SHCMD("pulsemixer --change-volume +1") },
-  { 0, XF86XK_AudioLowerVolume,	  spawn,		SHCMD("pulsemixer --change-volume -1") },
+  { 0, XF86XK_AudioRaiseVolume,	  spawn,		SHCMD("pamixer --allow-boost -i 3") },
+  { 0, XF86XK_AudioRaiseVolume,	  spawn,		SHCMD("pamixer --allow-boost -i 3") },
+	{ 0, XF86XK_AudioLowerVolume,	  spawn,		SHCMD("pamixer --allow-boost -d 3") },
+	
 
 	{ MODKEY|ShiftMask|ControlMask, XK_q,      quit,           {0} },
 
