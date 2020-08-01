ac8679e90bb02e797b97a45ddd897b742fc16e4e
Ghoyome <Ghoyome@protonmail.com>
2020-08-01 14:35:49 -0400

    dmenu seperation

--- config.def.h
+++ config.def.h
@@ -11,7 +11,6 @@ static const char *fonts[]          = {
     "DejaVu Sans Mono:pixelsize=12:antialias=true:autohint=true",
     "Font Awesome 5 Free:pixelsize=13:antialias=true:autohint=true", 
 };
-static const char dmenufont[]       = "DejaVu Sans Mono:pixelsize=12:antialias=true:autohint=true";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -79,7 +78,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run"};
 static const char *termcmd[]  = { "st", NULL };
 
 static const char scratchpadname[] = "scratchpad"; 
