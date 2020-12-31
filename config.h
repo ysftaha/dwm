f3c388178fa9178a13e07c0af81fa114d0baff0d
ysftaha <tahay@uwindsor.ca>
2020-12-30 19:02:57 -0500

    master fact

--- config.h
+++ config.h
@@ -4,13 +4,11 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 0;        /* snap pixel */
-static const int focusonwheel       = 0;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char statussep         = ';';      /* separator between status bars */
 static const char *fonts[]          = {
-    "DejaVu Sans Mono:pixelsize=12:antialias=true:autohint=true",
-    "Font Awesome 5 Free:pixelsize=13:antialias=true:autohint=true", 
+  "DejaVu Sans Mono:pixelsize=12:antialias=true:autohint=true",
+  "Font Awesome 5 Free:pixelsize=13:antialias=true:autohint=true", 
 };
 
 static const char col_gray1[]       = "#000000";
@@ -19,9 +17,9 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005743";
 static const char *colors[][3]      = {
-	/*                         fg         bg         border */
-	[SchemeNorm]         = { col_gray3, col_gray1,  col_gray1 },
-	[SchemeSel]          = { col_gray4, col_cyan,   col_cyan  },
+  /*                         fg         bg         border */
+  [SchemeNorm]         = { col_gray3, col_gray1,  col_gray1 },
+  [SchemeSel]          = { col_gray4, col_cyan,   col_cyan  },
   [SchemeTabActive]    = { col_gray2, col_gray1,  col_gray2 },
   [SchemeTabInactive]  = { col_gray4, col_gray1,  col_gray1 }
 };
@@ -31,9 +29,9 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 /* use xprop to set class */
 static const Rule rules[] = {
-	/* class             instance    title       tags mask     isfloating   monitor */
-	{ "brave-browser",   NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "Brave-browser",   NULL,       NULL,       1 << 2,       0,           -1 },
+  /* class             instance    title       tags mask     isfloating   monitor */
+  { "brave-browser",   NULL,       NULL,       1 << 2,       0,           -1 },
+  { "Brave-browser",   NULL,       NULL,       1 << 2,       0,           -1 },
 
   {"org.pwmt.zathura", NULL,       NULL,       1 << 1,       0,           -1 },
 
@@ -42,7 +40,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.33; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
@@ -56,22 +54,22 @@ static void (*bartabmonfns[])(Monitor *) = { monocle /* , customlayoutfn */ };
 static void (*bartabfloatfns[])(Monitor *) = { NULL /* , customlayoutfn */ };
 
 static const Layout layouts[] = {
-	/* symbol     arrange function */
+  /* symbol     arrange function */
   { "[deck]",       deck },
-	{ "[cMaster]", centeredmaster},
-	{ "[fullscr]", monocle },
-	{ "[tile]",       tile },
+  { "[cMaster]", centeredmaster},
+  { "[fullscr]", monocle },
+  { "[tile]",       tile },
   { "[column]",      col },
-	{ "[float]",      NULL },
+  { "[float]",      NULL },
 };
 
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG,VIEW) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY,                       VIEW,     toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             VIEW,     toggletag,      {.ui = 1 << TAG} },
+{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+{ MODKEY,                       VIEW,     toggleview,     {.ui = 1 << TAG} }, \
+{ MODKEY|ShiftMask,             VIEW,     toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -86,25 +84,25 @@ static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "128x40
 
 #include "movestack.c"
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+  /* modifier                     key        function        argument */
+  { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+  { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 
   /* windows */
-	{ MODKEY|ShiftMask,             XK_space,  zoom,           {0} },
-	{ MODKEY,                       XK_space,  switchcol,      {0} },
+  { MODKEY|ShiftMask,             XK_space,  zoom,           {0} },
+  { MODKEY,                       XK_space,  switchcol,      {0} },
 
   /* the stack */
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+  { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+  { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
   { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
   { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 
   /* the master window */
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.0125} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.0125} },
-	{ MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_d,      incnmaster,     {.i = -1 } },
+  { MODKEY,                       XK_h,      setmfact,       {.f = -0.0050} },
+  { MODKEY,                       XK_l,      setmfact,       {.f = +0.0050} },
+  { MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = +1 } },
+  { MODKEY|ShiftMask,             XK_d,      incnmaster,     {.i = -1 } },
 
   /* fact resize */
   { MODKEY|ControlMask,           XK_k,      setcfact,       {.f = +0.25} },
@@ -114,61 +112,61 @@ static Key keys[] = {
   /* Layouts */
   { MODKEY,                       XK_d,      setlayout,      {.v = &layouts[0]} }, // deck
   { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} }, // Master
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[3]} }, // Tile
+  { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[3]} }, // Tile
   { MODKEY,                       XK_c,      setlayout,      {.v = &layouts[4]} }, // Column
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} }, // Fullscr
+  { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} }, // Fullscr
 
   /* Tags */
-	TAGKEYS(XK_1,    0, XK_F1)
-	TAGKEYS(XK_2,    1, XK_F2)
-	TAGKEYS(XK_3,    2, XK_F3)
-	TAGKEYS(XK_4,    3, XK_F4)
-	TAGKEYS(XK_5,    4, XK_F5)
-	TAGKEYS(XK_6,    5, XK_F6)
-	TAGKEYS(XK_7,    6, XK_F7)
-	TAGKEYS(XK_8,    7, XK_F8)
-	TAGKEYS(XK_9,    8, XK_F9)
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+  TAGKEYS(XK_1,    0, XK_F1)
+    TAGKEYS(XK_2,    1, XK_F2)
+    TAGKEYS(XK_3,    2, XK_F3)
+    TAGKEYS(XK_4,    3, XK_F4)
+    TAGKEYS(XK_5,    4, XK_F5)
+    TAGKEYS(XK_6,    5, XK_F6)
+    TAGKEYS(XK_7,    6, XK_F7)
+    TAGKEYS(XK_8,    7, XK_F8)
+    TAGKEYS(XK_9,    8, XK_F9)
+    { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+    { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 
-  /* quick utils */
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_q,      killclient,     {0} },
- 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
-  { MODKEY,                       XK_Print,	 spawn,		       SHCMD("maim $HOME/sc-shot.png") },
-  // { MODKEY,                       XK_comma,  spawn,		       SHCMD("devtoggle --toggle \"DLL07BE:01 06CB:7A13 Touchpad\"")},
+    /* quick utils */
+    { MODKEY,                       XK_Tab,    view,           {0} },
+    { MODKEY,                       XK_b,      togglebar,      {0} },
+    { MODKEY,                       XK_q,      killclient,     {0} },
+    { MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
+    { MODKEY,                       XK_Print,	 spawn,		       SHCMD("maim $HOME/sc-shot.png") },
+    // { MODKEY,                       XK_comma,  spawn,		       SHCMD("devtoggle --toggle \"DLL07BE:01 06CB:7A13 Touchpad\"")},
 
-  /* Special keys */
-  { 0,                            XF86XK_MonBrightnessUp,	   spawn,		 SHCMD("bright up") },
-	{ 0,                            XF86XK_MonBrightnessDown,	 spawn,		 SHCMD("bright down") },
+    /* Special keys */
+    { 0,                            XF86XK_MonBrightnessUp,	   spawn,		 SHCMD("bright up") },
+    { 0,                            XF86XK_MonBrightnessDown,	 spawn,		 SHCMD("bright down") },
 
-  { 0,                            XF86XK_AudioMute,       	 spawn,		 SHCMD("vol mute") },
-  { 0,                            XF86XK_AudioRaiseVolume, 	 spawn,		 SHCMD("vol up") },
-  { 0,                            XF86XK_AudioLowerVolume,	 spawn,		 SHCMD("vol down") },
+    { 0,                            XF86XK_AudioMute,       	 spawn,		 SHCMD("vol mute") },
+    { 0,                            XF86XK_AudioRaiseVolume, 	 spawn,		 SHCMD("vol up") },
+    { 0,                            XF86XK_AudioLowerVolume,	 spawn,		 SHCMD("vol down") },
 
-  /* quit */
-	{ MODKEY|ShiftMask|ControlMask, XK_q,      quit,           {0} },
+    /* quit */
+    { MODKEY|ShiftMask|ControlMask, XK_q,      quit,           {0} },
 
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+    { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+    { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+    { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkClientWin,         MODKEY|ShiftMask,         Button1,        movemouse,      {0} },
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {.v = &layouts[5]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+  /* click                event mask      button          function        argument */
+  { ClkClientWin,         MODKEY|ShiftMask,         Button1,        movemouse,      {0} },
+  { ClkLtSymbol,          0,              Button1,        setlayout,      {.v = &layouts[5]} },
+  { ClkWinTitle,          0,              Button2,        zoom,           {0} },
+  { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+  { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+  { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+  { ClkTagBar,            0,              Button1,        view,           {0} },
+  { ClkTagBar,            0,              Button3,        toggleview,     {0} },
+  { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+  { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
--- dwm.c
+++ dwm.c
@@ -120,7 +120,6 @@ struct Monitor {
 	int nmaster;
 	int num;
 	int by;               /* bar geometry */
-	int eby;	      /* extra bar geometry */
 	int mx, my, mw, mh;   /* screen size */
 	int wx, wy, ww, wh;   /* window area  */
 	unsigned int seltags;
@@ -133,7 +132,6 @@ struct Monitor {
 	Client *stack;
 	Monitor *next;
 	Window barwin;
-	Window extrabarwin;
 	const Layout *lt[2];
   Pertag *pertag;
 };
@@ -172,6 +170,7 @@ static void detachstack(Client *c);
 static Monitor *dirtomon(int dir);
 static void drawbar(Monitor *m);
 static void drawbars(void);
+static void enternotify(XEvent *e);
 static void expose(XEvent *e);
 static void focus(Client *c);
 static void focusin(XEvent *e);
@@ -189,6 +188,7 @@ static void manage(Window w, XWindowAttributes *wa);
 static void mappingnotify(XEvent *e);
 static void maprequest(XEvent *e);
 static void monocle(Monitor *m);
+static void motionnotify(XEvent *e);
 static void movemouse(const Arg *arg);
 static Client *nexttiled(Client *c);
 static void pop(Client *);
@@ -248,7 +248,6 @@ static void centeredmaster(Monitor *m);
 /* variables */
 static const char broken[] = "broken";
 static char stext[256];
-static char estext[256];
 static int screen;
 static int sw, sh;           /* X display screen geometry width, height */
 static int bh, blw = 0;      /* bar geometry */
@@ -261,11 +260,13 @@ static void (*handler[LASTEvent]) (XEvent *) = {
 	[ConfigureRequest] = configurerequest,
 	[ConfigureNotify] = configurenotify,
 	[DestroyNotify] = destroynotify,
+	[EnterNotify] = enternotify,
 	[Expose] = expose,
 	[FocusIn] = focusin,
 	[KeyPress] = keypress,
 	[MappingNotify] = mappingnotify,
 	[MapRequest] = maprequest,
+	[MotionNotify] = motionnotify,
 	[PropertyNotify] = propertynotify,
 	[UnmapNotify] = unmapnotify
 };
@@ -550,8 +551,7 @@ buttonpress(XEvent *e)
 
 	click = ClkRootWin;
 	/* focus monitor if necessary */
-	if ((m = wintomon(ev->window)) && m != selmon
-	    && (focusonwheel || (ev->button != Button4 && ev->button != Button5))) {
+	if ((m = wintomon(ev->window)) && m != selmon) {
 		unfocus(selmon->sel, 1);
 		selmon = m;
 		focus(NULL);
@@ -576,10 +576,10 @@ buttonpress(XEvent *e)
 		else // Focus clicked tab bar item
 			bartabcalculate(selmon, x, TEXTW(stext) - lrpad + 2, ev->x, battabclick);
 	} else if ((c = wintoclient(ev->window))) {
- 		if (focusonwheel || (ev->button != Button4 && ev->button != Button5))
- 			focus(c);
- 		XAllowEvents(dpy, ReplayPointer, CurrentTime);
- 		click = ClkClientWin;
+		focus(c);
+		restack(selmon);
+		XAllowEvents(dpy, ReplayPointer, CurrentTime);
+		click = ClkClientWin;
 	}
 	for (i = 0; i < LENGTH(buttons); i++)
 		if (click == buttons[i].click && buttons[i].func && buttons[i].button == ev->button
@@ -637,9 +637,7 @@ cleanupmon(Monitor *mon)
 		m->next = mon->next;
 	}
 	XUnmapWindow(dpy, mon->barwin);
-	XUnmapWindow(dpy, mon->extrabarwin);
 	XDestroyWindow(dpy, mon->barwin);
-	XDestroyWindow(dpy, mon->extrabarwin);
 	free(mon);
 }
 
@@ -727,7 +725,6 @@ configurenotify(XEvent *e)
 					if (c->isfullscreen)
 						resizeclient(c, m->mx, m->my, m->mw, m->mh);
 				XMoveResizeWindow(dpy, m->barwin, m->wx, m->by, m->ww, bh);
-				XMoveResizeWindow(dpy, m->extrabarwin, m->wx, m->eby, m->ww, bh);
 			}
 			focus(NULL);
 			arrange(NULL);
@@ -911,11 +908,6 @@ drawbar(Monitor *m)
 		}
 	}
 	drw_map(drw, m->barwin, 0, 0, m->ww, bh);
-	if (m == selmon) { /* extra status is only drawn on selected monitor */
-		drw_setscheme(drw, scheme[SchemeNorm]);
-		drw_text(drw, 0, 0, mons->ww, bh, 0, estext, 0);
-		drw_map(drw, m->extrabarwin, 0, 0, m->ww, bh);
-	}
 }
 
 void
@@ -925,6 +917,25 @@ drawbars(void)
 
 	for (m = mons; m; m = m->next)
 		drawbar(m);
+}
+
+void
+enternotify(XEvent *e)
+{
+	Client *c;
+	Monitor *m;
+	XCrossingEvent *ev = &e->xcrossing;
+
+	if ((ev->mode != NotifyNormal || ev->detail == NotifyInferior) && ev->window != root)
+		return;
+	c = wintoclient(ev->window);
+	m = c ? c->mon : wintomon(ev->window);
+	if (m != selmon) {
+		unfocus(selmon->sel, 1);
+		selmon = m;
+	} else if (!c || c == selmon->sel)
+		return;
+	focus(c);
 }
 
 void
@@ -1098,7 +1109,7 @@ grabbuttons(Client *c, int focused)
 					XGrabButton(dpy, buttons[i].button,
 						buttons[i].mask | modifiers[j],
 						c->win, False, BUTTONMASK,
-						GrabModeSync, GrabModeSync, None, None);
+						GrabModeAsync, GrabModeSync, None, None);
 	}
 }
 
@@ -1279,6 +1290,23 @@ monocle(Monitor *m)
 		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[fullscr]");
 	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
 		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
+}
+
+void
+motionnotify(XEvent *e)
+{
+	static Monitor *mon = NULL;
+	Monitor *m;
+	XMotionEvent *ev = &e->xmotion;
+
+	if (ev->window != root)
+		return;
+	if ((m = recttomon(ev->x_root, ev->y_root, 1, 1)) != mon && mon) {
+		unfocus(selmon->sel, 1);
+		selmon = m;
+		focus(NULL);
+	}
+	mon = m;
 }
 
 void
@@ -1939,7 +1967,6 @@ togglebar(const Arg *arg)
 	selmon->showbar = selmon->pertag->showbars[selmon->pertag->curtag] = !selmon->showbar;
 	updatebarpos(selmon);
 	XMoveResizeWindow(dpy, selmon->barwin, selmon->wx, selmon->by, selmon->ww, bh);
-	XMoveResizeWindow(dpy, selmon->extrabarwin, selmon->wx, selmon->eby, selmon->ww, bh);
 	arrange(selmon);
 }
 
@@ -2095,22 +2122,14 @@ updatebars(void)
 	};
 	XClassHint ch = {"dwm", "dwm"};
 	for (m = mons; m; m = m->next) {
-		if (!m->barwin) {
-			m->barwin = XCreateWindow(dpy, root, m->wx, m->by, m->ww, bh, 0, DefaultDepth(dpy, screen),
-					CopyFromParent, DefaultVisual(dpy, screen),
-					CWOverrideRedirect|CWBackPixmap|CWEventMask, &wa);
-			XDefineCursor(dpy, m->barwin, cursor[CurNormal]->cursor);
-			XMapRaised(dpy, m->barwin);
-			XSetClassHint(dpy, m->barwin, &ch);
-		}
-		if (!m->extrabarwin) {
-			m->extrabarwin = XCreateWindow(dpy, root, m->wx, m->eby, m->ww, bh, 0, DefaultDepth(dpy, screen),
-					CopyFromParent, DefaultVisual(dpy, screen),
-					CWOverrideRedirect|CWBackPixmap|CWEventMask, &wa);
-			XDefineCursor(dpy, m->extrabarwin, cursor[CurNormal]->cursor);
-			XMapRaised(dpy, m->extrabarwin);
-			XSetClassHint(dpy, m->extrabarwin, &ch);
-		}
+		if (m->barwin)
+			continue;
+		m->barwin = XCreateWindow(dpy, root, m->wx, m->by, m->ww, bh, 0, DefaultDepth(dpy, screen),
+				CopyFromParent, DefaultVisual(dpy, screen),
+				CWOverrideRedirect|CWBackPixmap|CWEventMask, &wa);
+		XDefineCursor(dpy, m->barwin, cursor[CurNormal]->cursor);
+		XMapRaised(dpy, m->barwin);
+		XSetClassHint(dpy, m->barwin, &ch);
 	}
 }
 
@@ -2119,16 +2138,13 @@ updatebarpos(Monitor *m)
 {
 	m->wy = m->my;
 	m->wh = m->mh;
-	m->wh -= bh * m->showbar * 2;
-	m->wy = m->showbar ? m->wy + bh : m->wy;
 
 	if (m->showbar) {
-    m->by = m->topbar ? m->wy - bh : m->wy + m->wh;
-    m->eby = m->topbar ? m->wy + m->wh : m->wy - bh;
-  } else {
+    m->wh -= bh;
+    m->by = m->topbar ? m->wy : m->wy + m->wh;
+    m->wy = m->topbar ? m->wy + bh : m->wy;
+  } else
 		m->by = -bh;
-		m->eby = -bh;
-  }
 }
 
 void
@@ -2285,21 +2301,11 @@ updatesizehints(Client *c)
 void
 updatestatus(void)
 {
-  char text[512];
-  if (!gettextprop(root, XA_WM_NAME, text, sizeof(text))) {
-    strcpy(stext, "dwm-"VERSION);
-    estext[0] = '\0';
-  } else {
-    char *e = strchr(text, statussep);
-    if (e) {
-      *e = '\0'; e++;
-      strncpy(estext, e, sizeof(estext) - 1);
-    } else {
-      estext[0] = '\0';
-    }
-    strncpy(stext, text, sizeof(stext) - 1);
+  if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext))) {
+    // strcpy(stext, "dwm-"VERSION); // FIXME
   }
   drawbar(selmon);
+
 }
 
 void
@@ -2403,7 +2409,7 @@ wintomon(Window w)
 	if (w == root && getrootptr(&x, &y))
 		return recttomon(x, y, 1, 1);
 	for (m = mons; m; m = m->next)
-	if (w == m->barwin || w == m->extrabarwin)
+	if (w == m->barwin)
 			return m;
 	if ((c = wintoclient(w)))
 		return c->mon;
